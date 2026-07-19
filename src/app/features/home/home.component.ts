8f6bb20d6c03c85a68858aa6ba34d041ff8aa5c7
Nereius8 <nelviralopez@gmail.com>
2026-07-19 17:46:22 +0200

    feat: "create home component"

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private router = inject(Router);
 
+  heroImage = 'tabalas.jpg';
+
+  navigateToRental(): void {
+    this.router.navigate(['/rental']);
+  }
 }
