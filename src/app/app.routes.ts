588a336024fa9c3b05c6065b57bb14e60ba27776
Nereius8 <nelviralopez@gmail.com>
2026-07-27 12:19:07 +0200

    "chore: configuration of core routing providers and root navigation shell"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { RentalListComponent } from './features/rental/rental-list/rental-list.component';
+import { LessonsComponent } from './features/lessons/lessons.component';
+import { TransportComponent } from './features/transport/transport.component';
+import { LocationComponent } from './features/location/location.component';
 
 export const routes: Routes = [
 
-    { 
-        path: '', 
-        component: HomeComponent 
-      },
+  {  path: '',redirectTo: 'home', pathMatch: 'full' },  
+  
+  { path: 'home', component: HomeComponent   },
 
-      { 
-        path: 'rental', 
-        loadComponent: () => import('./features/rental/rental-list/rental-list.component').then(m => m.RentalListComponent)
-      },
+  {  path: 'rental', component: RentalListComponent },
 
-      { 
-        path: '**', 
-        redirectTo: '' 
-      }
+  { path: 'lesson', component: LessonsComponent },
+
+  { path: 'transport', component: TransportComponent },
+
+  { path: 'location', component: LocationComponent },
+
+   { path: '**', redirectTo: 'home'  }
 ];
