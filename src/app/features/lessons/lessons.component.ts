951ed4925d5ba73b4bdb009afe9a947b6088518b
Nereius8 <nelviralopez@gmail.com>
2026-07-28 17:25:21 +0200

    "fix(lessons): fix routing path matching and update header navigation links"

--- src/app/features/lessons/lessons.component.ts
+++ src/app/features/lessons/lessons.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lessons',
+  standalone: true,
   imports: [],
   templateUrl: './lessons.component.html',
   styleUrl: './lessons.component.css'
